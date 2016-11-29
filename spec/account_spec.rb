827a78947bc9cfe027ecc8dbc724e440d09fbd2c
alexanderwjrussell <awjrussell@live.co.uk>
2016-11-29 17:13:44 +0000

    Testing 1) New test for history on the Account 2) Tests for ability to record credits and debits on transaction_history 3) Tests failing

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe Account do
 
+  let(:transaction_history) {spy :transaction_history, :new => nil, :history => [], :record_credit => nil, :record_debit => nil}
+
+
   it "can return a balance" do
     expect(subject.balance).to eq(0)
   end
@@ -19,6 +22,21 @@ describe Account do
 
   it "can't have money debited if insufficient funds" do
     expect { subject.debit(10) }.to raise_error("Insufficient funds for transaction")
+  end
+
+  it "has a transaction_history on initialize" do
+    expect(subject.get_history).to eq([])
+  end
+
+  it "can record credits in transaction_history" do
+    subject.credit(10)
+    expect(transaction_history).to have_received(:record_credit).with(10,10)
+  end
+
+  it "can record debits in transaction_history" do
+    subject.credit(10)
+    subject.debit(5)
+    expect(transaction_history).to have_received(:record_debit).with(5,5)
   end
 
 end
